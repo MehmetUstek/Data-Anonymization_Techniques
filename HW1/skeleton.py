ffae37541ebbe4035e05f7d8853459d2ef47c025
MehmetUstek <mustek17@ku.edu.tr>
2021-10-28 17:17:51 +0300

    Random Anonymizer Done

--- HW1/skeleton.py
+++ HW1/skeleton.py
@@ -19,6 +19,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 5:
     sys.stdout.write("Requires Python 3.x.\n")
     sys.exit(1)
 
+
 ##############################################################################
 # Helper Functions                                                           #
 # These functions are provided to you as starting points. They may help your #
@@ -54,7 +55,7 @@ def write_dataset(dataset, dataset_file: str) -> bool:
     Returns:
         bool: True if succeeds.
     """
-    assert len(dataset)>0, "The anonymized dataset is empty."
+    assert len(dataset) > 0, "The anonymized dataset is empty."
     keys = dataset[0].keys()
     with open(dataset_file, 'w', newline='')  as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
@@ -79,13 +80,13 @@ def read_DGH(DGH_file: str):
 
     tree = Tree()
     total_children = 1
-    parent = tree.create_node(result[0][0],"Any", data=0)
+    parent = tree.create_node(result[0][0].rstrip(), 'Any', data=0)
     root = parent
     i = 0
     total_children += 1
     indentation_level = 0
     for c in result:
-        if len(c) ==1:
+        if len(c) == 1:
             pass
         else:
             c_count = 1
@@ -98,21 +99,25 @@ def read_DGH(DGH_file: str):
             # indentation_level = len(c)
             # print(indentation_level)
         # Top down
-        if i!= 0:
+        if i != 0:
             if parent.is_root():
-                parent = tree.create_node(result[i][1].rstrip(),identifier=result[i][1].rstrip(), parent=root, data=indentation_level)
+                parent = tree.create_node(result[i][1].rstrip(), identifier=result[i][1].rstrip(), parent=root,
+                                          data=indentation_level)
             else:
                 if indentation_level > parent.data:
-                    parent = tree.create_node(result[i][indentation_level-1].rstrip(),identifier=result[i][indentation_level-1].rstrip(), parent=parent, data=indentation_level)
+                    parent = tree.create_node(result[i][indentation_level - 1].rstrip(),
+                                              identifier=result[i][indentation_level - 1].rstrip(), parent=parent,
+                                              data=indentation_level)
                 else:
                     if parent.data:
                         # Bottom up
                         while indentation_level <= parent.data:
                             parent = tree.parent(parent.identifier)
-                        parent = tree.create_node(result[i][indentation_level-1].rstrip(),identifier=result[i][indentation_level-1].rstrip(), parent=parent,data = indentation_level)
+                        parent = tree.create_node(result[i][indentation_level - 1].rstrip(),
+                                                  identifier=result[i][indentation_level - 1].rstrip(), parent=parent,
+                                                  data=indentation_level)
 
         i += 1
-
 
     # tree.show()
     result = tree
@@ -145,7 +150,7 @@ def read_DGHs(DGH_folder: str) -> dict:
 
 
 def cost_MD(raw_dataset_file: str, anonymized_dataset_file: str,
-    DGH_folder: str) -> float:
+            DGH_folder: str) -> float:
     """Calculate Distortion Metric (MD) cost between two datasets.
 
     Args:
@@ -158,23 +163,23 @@ def cost_MD(raw_dataset_file: str, anonymized_dataset_file: str,
     """
     raw_dataset = read_dataset(raw_dataset_file)
     anonymized_dataset = read_dataset(anonymized_dataset_file)
-    assert(len(raw_dataset)>0 and len(raw_dataset) == len(anonymized_dataset)
-        and len(raw_dataset[0]) == len(anonymized_dataset[0]))
+    assert (len(raw_dataset) > 0 and len(raw_dataset) == len(anonymized_dataset)
+            and len(raw_dataset[0]) == len(anonymized_dataset[0]))
     DGHs = read_DGHs(DGH_folder)
 
-    #TODO: complete this function.
+    # TODO: complete this function.
     # print(DGHs)
     total_MD_cost = 0
     ctr_raw = Counter()
     for data in raw_dataset:
         for df, sa in data.items():
-            ctr_raw[(df,sa)] += 1
+            ctr_raw[(df, sa)] += 1
     # print(ctr_raw)
 
     ctr_anon = Counter()
     for data in anonymized_dataset:
         for df, sa in data.items():
-            ctr_anon[(df,sa)] += 1
+            ctr_anon[(df, sa)] += 1
     # print(ctr_anon)
     generalized = ctr_anon - ctr_raw
     lost = ctr_raw - ctr_anon
@@ -194,7 +199,7 @@ def cost_MD(raw_dataset_file: str, anonymized_dataset_file: str,
         list_dgh.append(dgh)
         list_tree.append(tree)
     tree1 = Tree()
-    for (i,j),k in ctr_anon.items():
+    for (i, j), k in ctr_anon.items():
         if k != 0:
             # print("i", i)
             # print("j",j)
@@ -207,13 +212,13 @@ def cost_MD(raw_dataset_file: str, anonymized_dataset_file: str,
                 level_of_the_deepest = current.data - 1
                 # Get the hierarchical parent until the j_wanted is equal to create a (i,j) pair that is in the ctr_anon dict.
                 j_wanted = ""
-                while not ctr_anon.get((i,j_wanted)):
+                while not ctr_anon.get((i, j_wanted)):
                     parent = tree1.parent(current.identifier)
                     current = parent
                     j_wanted = current.identifier
                     # j_temp -= 1
-                ctr_anon[(i,j_wanted)] += k
-                ctr_anon[(i,j)] -= k
+                ctr_anon[(i, j_wanted)] += k
+                ctr_anon[(i, j)] -= k
                 # k represents the every to be deleted data. So it needs to be multiplied.
                 # print(ctr_anon)
                 level_of_farthest = current.data
@@ -229,7 +234,7 @@ def cost_MD(raw_dataset_file: str, anonymized_dataset_file: str,
 
 
 def cost_LM(raw_dataset_file: str, anonymized_dataset_file: str,
-    DGH_folder: str) -> float:
+            DGH_folder: str) -> float:
     """Calculate Loss Metric (LM) cost between two datasets.
 
     Args:
@@ -242,8 +247,8 @@ def cost_LM(raw_dataset_file: str, anonymized_dataset_file: str,
     """
     raw_dataset = read_dataset(raw_dataset_file)
     anonymized_dataset = read_dataset(anonymized_dataset_file)
-    assert(len(raw_dataset)>0 and len(raw_dataset) == len(anonymized_dataset)
-        and len(raw_dataset[0]) == len(anonymized_dataset[0]))
+    assert (len(raw_dataset) > 0 and len(raw_dataset) == len(anonymized_dataset)
+            and len(raw_dataset[0]) == len(anonymized_dataset[0]))
     DGHs = read_DGHs(DGH_folder)
 
     total_LM_cost = 0.0
@@ -262,7 +267,7 @@ def cost_LM(raw_dataset_file: str, anonymized_dataset_file: str,
     tree1 = Tree()
 
     LM_cost = 0
-    for (i,j),k in ctr_raw.items():
+    for (i, j), k in ctr_raw.items():
         if k != 0:
             if i in list_dgh:
                 index = list_dgh.index(i)
@@ -281,23 +286,16 @@ def cost_LM(raw_dataset_file: str, anonymized_dataset_file: str,
                     a_list = a_list[1:]
                     print(a_list)
                     number_of_descendant = len(a_list)
-                    LM_cost = float((number_of_descendant - 1) / (size -1))
+                    LM_cost = float((number_of_descendant - 1) / (size - 1))
                     LM_cost_record = LM_cost * 1 / num_of_QIs
                     total_LM_cost += LM_cost_record
-
-
-
 
     print(total_LM_cost)
     return total_LM_cost
 
 
-
-
-
-
 def random_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
-    output_file: str):
+                      output_file: str):
     """ K-anonymization a dataset, given a set of DGHs and a k-anonymity param.
 
     Args:
@@ -310,7 +308,6 @@ def random_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
     DGHs = read_DGHs(DGH_folder)
 
     anonymized_dataset = []
-    #TODO: complete this function.
 
     # Given a dataset, randomly divide the records in D, into clusters of size k
     list_of_clustered_records = []
@@ -321,7 +318,7 @@ def random_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
     record_counter = 0
     dict_of_clustered_records = defaultdict(list)
     for record in raw_dataset:
-        rand_val = random.randint(0, number_of_clusters-1)
+        rand_val = random.randint(0, number_of_clusters - 1)
         # if not dict_of_clustered_records.get(random)
         if dict_of_clustered_records.get(rand_val):
             while len(dict_of_clustered_records.get(rand_val)) == k:
@@ -339,30 +336,33 @@ def random_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
     # print(dict_of_clustered_records)
     key_string = ""
     lss = {}
-    i= 0
+    i = 0
     for dgh in DGHs.keys():
         print(dgh)
         lss[i] = dgh
         i += 1
         # key_string += dgh + ","
+
+    # anonymized_dataset.append(lss)
+
     for equivalence_class in dict_of_clustered_records.values():
-        print(equivalence_class)
+        # print(equivalence_class)
         # while k_anonymity(equivalence_class, k):
         #     pass
-        k_anonymity(equivalence_class, k, DGHs)
+        equivalence_class = k_anonymity(equivalence_class, k, DGHs)
+        for item in equivalence_class:
+            anonymized_dataset.append(item)
 
 
-    key_string = key_string[:-1]
-    anonymized_dataset.append(lss)
-    print(key_string)
+    # print(key_string)
     # for i in dict_of_clustered_records:
     #     anonymized_dataset.append(dict_of_clustered_records[i])
 
     # Data is clustered into chunks of k records.
     # k-anonymity
 
-
     write_dataset(anonymized_dataset, output_file)
+
 
 def k_anon(equivalence_class, DGHs):
     counter = Counter()
@@ -371,39 +371,56 @@ def k_anon(equivalence_class, DGHs):
             counter[(dgh, item[dgh])] += 1
     least_common = counter.most_common()[-1]
     return least_common[1]
+
+
 def is_k_anon(equivalence_class, k, DGHs):
     if k_anon(equivalence_class, DGHs) >= k:
         return True
     return False
 
+
 def k_anonymity(equivalence_class, k, DGHs):
-    counter = Counter()
-    for item in equivalence_class:
-        for dgh in DGHs.keys():
-            counter[(dgh,item[dgh])] += 1
-    print(counter)
-    value = 0
-    print("k-anon")
-    print(is_k_anon(equivalence_class,k, DGHs))
-    print(k_anon(equivalence_class,k,DGHs))
-    new_counter = Counter()
-    for key, value in counter.items():
-        while not int(value) >= k:
-            tree = DGHs[key[0]]
 
-            node = tree.get_node(key[1])
-            counter[(key,value)] -= 1
+    while not is_k_anon(equivalence_class, k, DGHs):
+        counter = Counter()
+        for item in equivalence_class:
+            for dgh in DGHs.keys():
+                counter[(dgh, item[dgh])] += 1
+        i=0
+        key = counter.most_common()[-1][0]
+        # value = counter.most_common()[-1][1]
+        while key[1] == 'Any':
+            key = counter.most_common()[-1 -i][0]
+            i += 1
+        # for key, value in reversed(counter.most_common()):
+        #     if is_k_anon(equivalence_class, k, DGHs):
+        #         break
+        # if value >= k:
+        #     continue
+            # Problem:
+            # The function goes into while loop, since we have 1 Any, and 5 United States lets say.
+        tree = DGHs[key[0]]
+
+        node = tree.get_node(key[1])
+        # counter[(key,value)] -= 1
+        if not node.is_root():
             node = tree.parent(node.identifier)
-            new_counter[(dgh,node.identifier)] += 1
-            value += 1
+        else:
+            continue
+        new_data = node.tag
+        # equivalence_class[]
+        # value += 1
+        for item in equivalence_class:
+            if item[key[0]] == key[1]:
+                item[key[0]] = new_data
 
-            # pass
-            # DGHs[]
-            print(counter)
+    # for item in equivalence_class:
+    #     print(item)
+    return equivalence_class
 
 
 def clustering_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
-    output_file: str):
+                          output_file: str):
     """ Clustering-based anonymization a dataset, given a set of DGHs.
 
     Args:
@@ -416,13 +433,13 @@ def clustering_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
     DGHs = read_DGHs(DGH_folder)
 
     anonymized_dataset = []
-    #TODO: complete this function.
+    # TODO: complete this function.
 
     write_dataset(anonymized_dataset, output_file)
 
 
 def topdown_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
-    output_file: str):
+                       output_file: str):
     """ Top-down anonymization a dataset, given a set of DGHs.
 
     Args:
@@ -435,14 +452,15 @@ def topdown_anonymizer(raw_dataset_file: str, DGH_folder: str, k: int,
     DGHs = read_DGHs(DGH_folder)
 
     anonymized_dataset = []
-    #TODO: complete this function.
+    # TODO: complete this function.
 
     write_dataset(anonymized_dataset, output_file)
+
 
 # print(read_DGHs("DGHs"))
 # cost_MD("adult-hw1.csv","adult-anonymized.csv", "DGHs" )
 # cost_LM("adult-anonymized.csv","adult-anonymized.csv", "DGHs" )
-random_anonymizer('adult_small.csv',"DGHs",3,'adult-random-anonymized.csv')
+# random_anonymizer('adult-hw1.csv', "DGHs", 3, 'adult-random-anonymized.csv')
 
 # Command line argument handling and calling of respective anonymizer:
 if len(sys.argv) < 6:
@@ -465,8 +483,7 @@ function(raw_file, dgh_path, k, anonymized_file)
 
 cost_md = cost_MD(raw_file, anonymized_file, dgh_path)
 cost_lm = cost_LM(raw_file, anonymized_file, dgh_path)
-print (f"Results of {k}-anonimity:\n\tCost_MD: {cost_md}\n\tCost_LM: {cost_lm}\n")
-
+print(f"Results of {k}-anonimity:\n\tCost_MD: {cost_md}\n\tCost_LM: {cost_lm}\n")
 
 # Sample usage:
 # python3 code.py clustering DGHs/ adult-hw1.csv result.csv 300
